d2a0e8fcaf2dc436fd7e02339dbaac2db7f89c43
Vladimir Volkov <53047099+ApeIsHere@users.noreply.github.com>
2025-03-14 18:34:43 +0300

    feat: add AddForm virtualization

--- src/components/AddForm/AddForm.tsx
+++ src/components/AddForm/AddForm.tsx
@@ -1,12 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../../store";
 import { useState } from "react";
+import { AutoSizer, List } from "react-virtualized";
 import { v4 as uuidv4 } from "uuid";
-import { addAsset } from "../../features/portfolio/portfolioSlice";
+import { AppDispatch, RootState } from "../../store";
 import { Asset } from "../../types";
+import { addAsset } from "../../features/portfolio/portfolioSlice";
 import AssetItem from "../common/AssetItem";
 import Button from "../common/Button";
 import Input from "../common/Input";
+import {
+  ADD_FORM_LIST_HEIGHT,
+  ROW_HEIGHT,
+  OVERSCAN_ROW_COUNT,
+} from "../../constants/appConstants";
 
 interface AddFormProps {
   onClose: () => void;
@@ -36,6 +42,28 @@ const AddForm: React.FC<AddFormProps> = ({ onClose }) => {
     }
   };
 
+  // Function to render visible rows with react-virtualized
+  const rowRenderer = ({
+    index,
+    key,
+    style,
+  }: {
+    index: number;
+    key: string;
+    style: React.CSSProperties;
+  }) => {
+    const asset = filteredAssets[index];
+    return (
+      <div key={key} style={style}>
+        <AssetItem
+          asset={asset}
+          onClick={() => setSelectedAsset(asset)}
+          selected={selectedAsset?.id === asset.id}
+        />
+      </div>
+    );
+  };
+
   const filteredAssets = marketAsset.filter((asset) =>
     asset.name.toLowerCase().includes(searchInput.toLocaleLowerCase())
   );
@@ -49,18 +77,21 @@ const AddForm: React.FC<AddFormProps> = ({ onClose }) => {
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
           placeholder="Search assets..."
-        ></Input>
-        <ul className="asset-list">
-          {filteredAssets.map((asset) => (
-            <li key={asset.id}>
-              <AssetItem
-                asset={asset}
-                onClick={() => setSelectedAsset(asset)}
-                selected={selectedAsset?.id === asset.id}
+        />
+        <div className="asset-list" style={{ height: ADD_FORM_LIST_HEIGHT }}>
+          <AutoSizer>
+            {({ height, width }) => (
+              <List
+                width={width}
+                height={height}
+                rowCount={filteredAssets.length}
+                rowHeight={ROW_HEIGHT}
+                rowRenderer={rowRenderer}
+                overscanRowCount={OVERSCAN_ROW_COUNT}
               />
-            </li>
-          ))}
-        </ul>
+            )}
+          </AutoSizer>
+        </div>
       </div>
       {selectedAsset && (
         <>
