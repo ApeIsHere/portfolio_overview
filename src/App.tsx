8fbc85dfe54057bdad6f896294f4e52c002a1cd4
Vladimir Volkov <53047099+ApeIsHere@users.noreply.github.com>
2025-03-14 23:27:21 +0300

    style: update globalStyles and App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
+import useWebSocket from "./hooks/useWebsocket";
 import AddForm from "./components/AddForm/AddForm";
-import PortfolioList from "./components/PortfolioList/PortfolioList";
 import Modal from "./components/Modal/Modal";
 import Button from "./components/common/Button";
-import useWebSocket from "./hooks/useWebsocket";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -11,11 +11,14 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Portfolio Overview</h1>
-      <Button variant="primary" onClick={() => setIsModalOpen(true)}>
-        Add Asset
-      </Button>
-      <PortfolioList />
+      <div className="tablet-container">
+        <div className="add-button-container">
+          <Button variant="primary" onClick={() => setIsModalOpen(true)}>
+            Add Asset
+          </Button>
+        </div>
+        <Dashboard />
+      </div>
 
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <AddForm onClose={() => setIsModalOpen(false)} />
